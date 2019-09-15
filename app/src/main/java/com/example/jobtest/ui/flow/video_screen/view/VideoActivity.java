4ffa2b60106313bde0d64609b8ca583c7b9ba6ee
Hofit Mizrahi <hofitmizrahi92@gmail.com>
2019-09-15 22:39:53 +0300

    commit finish

--- app/src/main/java/com/example/jobtest/ui/flow/video_screen/view/VideoActivity.java
+++ app/src/main/java/com/example/jobtest/ui/flow/video_screen/view/VideoActivity.java
@@ -3,9 +3,17 @@ package com.example.jobtest.ui.flow.video_screen.view;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.jobtest.R;
 import com.example.jobtest.application.JobTestApplication;
@@ -16,11 +24,17 @@ import com.example.jobtest.ui.base.BasePresenter;
 import com.example.jobtest.ui.flow.video_screen.contract.VideoContract;
 import com.example.jobtest.ui.flow.video_screen.presenter.VideoPresenter;
 
+import java.util.Objects;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
 
-public class VideoActivity extends BaseActivity implements VideoContract.View, MediaPlayer.OnInfoListener {
+public class VideoActivity extends BaseActivity implements VideoContract.View, MediaPlayer.OnInfoListener, MediaPlayer.OnPreparedListener {
+
+    public final String VIDEO_STATE = "video_state";
+
+    private int mVideoCurrentPosition;
 
     @Inject
     String videoUrl;
@@ -30,6 +44,10 @@ public class VideoActivity extends BaseActivity implements VideoContract.View, M
 
     @BindView(R.id.video_view)
     VideoView mVideoView;
+
+    @Nullable
+    @BindView(R.id.rotate_image)
+    ImageView mImage;
 
     @Override
     protected void initDependencies() {
@@ -41,12 +59,12 @@ public class VideoActivity extends BaseActivity implements VideoContract.View, M
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        try {
-            getSupportActionBar().hide();
-        } catch (NullPointerException ee) {
-            ee.printStackTrace();
+
+        if (savedInstanceState != null) {
+
+            mVideoCurrentPosition = savedInstanceState.getInt(VIDEO_STATE);
         }
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -60,7 +78,38 @@ public class VideoActivity extends BaseActivity implements VideoContract.View, M
         mVideoView.setMediaController(new MediaController(this));
         mVideoView.setVideoURI(Uri.parse(videoUrl));
         mVideoView.setOnInfoListener(this);
+        mVideoView.setOnPreparedListener(this);
+        mVideoView.requestFocus();
         mVideoView.start();
+
+        if(mImage != null) {
+            rotateAnimation();
+        }
+
+    }
+
+    private void rotateAnimation() {
+
+        RotateAnimation rotate = new RotateAnimation(0, 90,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        rotate.setDuration(4000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        mImage.setAnimation(rotate);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle) {
+        bundle.putInt(VIDEO_STATE, mVideoCurrentPosition);
+
+        super.onSaveInstanceState(bundle);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVideoCurrentPosition = mVideoView.getCurrentPosition();
     }
 
     @Override
@@ -77,5 +126,11 @@ public class VideoActivity extends BaseActivity implements VideoContract.View, M
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mVideoView.seekTo(mVideoCurrentPosition);
+        mVideoView.start();
     }
 }
